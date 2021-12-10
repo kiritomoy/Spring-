06f421e2b7eba0cad4fe7d4d647311b862fe086f
Kiritomoy <Kiritomoy@outlook.com>
2021-12-11 00:19:48 +0800

    测试demo

---
+++ 01-SpringBootPro/src/main/java/com/mo/web/MyController.java
@@ -0,0 +1,15 @@
+package com.mo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello spring boot!";
+    }
+}
